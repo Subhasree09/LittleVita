61c3aa51957104423f9a659deea091332e087121
Soumen3 <soumensamanta112233@gmail.com>
2024-03-03 22:36:52 +0530

    pages are created

--- Base/views.py
+++ Base/views.py
@@ -4,3 +4,26 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'Base/home.html')
 
+def nutrition(request):
+    return render(request, 'Base/nutrition.html')
+
+def awarness(request):
+    return render(request, 'Base/awarness.html')
+
+def blog(request):
+    return render(request, 'Base/blog.html')
+
+def contact(request):
+    return render(request, 'Base/contact.html')
+
+def doctors(request):
+    return render(request, 'Base/doctors.html')
+
+def disease(request):
+    return render(request, 'Base/disease.html')
+
+def vaccine(request):
+    return render(request, 'Base/vaccine.html')
+
+def videos(request):
+    return render(request, 'Base/videos.html')
