5437c7f8cf35c93bbe9fb05f2b01b1b64686c64c
Soumen3 <soumensamanta112233@gmail.com>
2024-03-10 06:16:16 +0530

    message design

--- Base/static/Base/js/script.js
+++ Base/static/Base/js/script.js
@@ -1,1 +1,26 @@
 console.log("Hello from script.js!");
+// setTimeout(function() {
+// 	var errorElement = document.querySelector('.error');
+// 	if (errorElement) {
+// 	  location.reload(true);
+// 	}
+//   }, 5000);
+//   setTimeout(function(){
+// 	var successElement=document.querySelector('.success');
+// 	if(successElement){
+// 	  location.reload(true);
+// 	}
+//   },5000);
+
+document.querySelector('.cancel-message').addEventListener('click', function() {
+    var messageBox = document.querySelector('.message-box');
+    messageBox.style.display = 'block'; // Show the message box
+	messageBox.style.display = 'none';
+    
+});
+
+setTimeout(function(){
+	var messageBox = document.querySelector('.message-box');
+	messageBox.style.display = 'block'; // Show the message box
+	messageBox.style.display = 'none';
+},3000);
